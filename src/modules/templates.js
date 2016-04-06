abec88d4eb02bd79b36deb48947a6a7b480836c3
Mo <Radiergummi@users.noreply.github.com>
2016-04-06 22:02:35 +0200

    Refactored the templates module to use promises

--- src/modules/templates.js
+++ src/modules/templates.js
@@ -11,58 +11,56 @@
     return console.error('[modules/templates] app.js has not been loaded yet');
   }
 
-  var templatesModule = {
-    // the options storage
-    options: {
-      templateUrl:    '/xorb/test/templates/',
-      templateEngine: 'mustache'
-    },
+  var engineReadyPromise,
+      templatesModule = {
+        // the options storage
+        options: {
+          templateUrl:    'templates/',
+          templateEngine: 'mustache'
+        },
 
-    // the template cache
-    cache: {},
+        // the template cache
+        cache: {},
 
-    templateEngine: null,
-    renderMethod: null
-  };
+        templateEngine: null,
+        renderMethod: null
+      };
 
   switch (templatesModule.options.templateEngine) {
     case 'mustache':
-      app.http.getScript('/src/lib/mustache.js', function(error) {
-        if (error) {
-          return console.error('[modules/templates] Mustache.js could not be fetched.');
-        }
-
+      engineReadyPromise = app.http.getScript('/src/lib/mustache.js').then(function() {
         templatesModule.templateEngine = window.Mustache;
         templatesModule.renderMethod = function(engine, template, variables) {
           return engine.render(template, variables);
         };
+      }, function(error) {
+        return console.error('[modules/templates] Mustache.js could not be fetched.');
       });
       break;
 
     case 'handlebars':
-      app.http.getScript('/src/lib/handlebars.js', function(error) {
-        if (error) {
-          return console.error('[modules/templates] Handlebars.js could not be fetched.');
-        }
-
+      engineReadyPromise = app.http.getScript('/src/lib/handlebars.js').then(function() {
         templatesModule.templateEngine = window.Handlebars.noConflict();
         templatesModule.renderMethod = function(engine, template, variables) {
           return engine.compile(template)(variables);
         };
+      }, function(error) {
+        return console.error('[modules/templates] Handlebars.js could not be fetched.');
       });
       break;
 
     case 'templates.js':
-      app.http.getScript('/src/lib/templates.js', function(error) {
-        if (error) {
-          return console.error('[modules/templates] Templates.js could not be fetched.');
-        }
-
+      engineReadyPromise = app.http.getScript('/src/lib/templates.js').then(function() {
         templatesModule.templateEngine = window.templates;
         templatesModule.renderMethod = function(engine, template, variables) {
-          console.log('parsing ' + template);
           return engine.parse(template, variables);
         };
+      }, function(error) {
+        return console.error('[modules/templates] Templates.js could not be fetched.');
+      });
+    default:
+      engineReadyPromise = new Promise(function(resolve, reject) {
+        reject('There is no engine available by that name');
       });
   }
 
@@ -75,13 +73,13 @@
   templatesModule.configure = function(options) {
   };
 
-  
+
   /**
    * renders a template string and returns a promise
-   * 
+   *
    * @param {string} template   the template string to render
    * @param {object} variables  the template variables to use
-   * @returns {Promise} 
+   * @returns {Promise}
    */
   templatesModule.render = function(template, variables) {
     return new Promise(function(resolve, reject) {
@@ -94,39 +92,45 @@
   };
 
 
-  // load the module
-  app.registerModule('templates', templatesModule, function() {
+  engineReadyPromise.then(function() {
+    // load the module
+    app.registerModule('templates', templatesModule, function() {
 
-    // expose the render method
-    app.mountModuleEndpoint('render', 'templates', 'render');
+      // expose the render method
+      app.mountModuleEndpoint('render', 'templates', 'render');
 
-    /**
-     * insert the getTemplate method into the HTTP API. The reason why this is not a part
-     * of the Xorb core is that users should have the freedom of (not) using the templates
-     * module.
-     *
-     * @param url
-     * @param callback
-     * @param params
-     * @param headers
-     * @param templateVariables
-     */
-    app.http.getTemplate = function(url, callback, templateVariables, params, headers) {
+      /**
+       * insert the getTemplate method into the HTTP API. The reason why this is not a part
+       * of the Xorb core is that users should have the freedom of (not) using the templates
+       * module.
+       *
+       * @param url
+       * @param callback
+       * @param params
+       * @param headers
+       * @param templateVariables
+       */
+      app.http.getTemplate = function(url, callback, templateVariables, params, headers) {
 
-      // if this is not an absolute link, attach our base path
-      if (url.substring(0, 7) !== 'http://' || url.substring(0, 8) !== 'https://') {
-        url = templatesModule.options.templateUrl + url;
+        // if this is not an absolute link, attach our base path
+        if (! /^https?:\/\//i.test(url)) {
+          url = templatesModule.options.templateUrl + url;
+        }
+
+        var renderedTemplate = this.get(url, null, params, headers)
+          .then(response => response.text(), function(error) {
+            console.error('[modules/templates] Could not fetch template: ' + error.message);
+          })
+          .then(function(response) {
+            return app.render(response, templateVariables);
+          });
+
+        if (callback) {
+          return renderedTemplate.then(callback);
+        }
+
+        return renderedTemplate;
       }
-
-      this.get(url, null, params, headers)
-        .then(response => response.text())
-        .then(function(response) {
-          return app.render(response, templateVariables);
-        })
-        .then(callback)
-        .catch(function(error) {
-          console.error('[modules/templates] Could not fetch template: ' + error.message);
-        });
-    }
+    });
   });
 })();
